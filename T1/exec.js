c7e4c90e6248fe33f28a7970cce3c5d5d9ae81d8
Gabriel Martins da Costa Medeiros <67205923+gabmm@users.noreply.github.com>
2024-12-02 21:21:51 -0300

    feat: cam primeira pessoa, e mudança de perpectiva

--- T1/exec.js
+++ T1/exec.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import {PointerLockControls} from '../build/jsm/controls/PointerLockControls.js';
 import {
     initRenderer,
     initCamera,
@@ -15,20 +16,58 @@ import Voxel, { MATERIALS } from './voxel.js'
 import { Material, Vector2, Vector3 } from '../build/three.module.js';
 
 const VX = 10;
+const SPEED = 2;
+const FLY_FACTOR = .5;
 
 let scene, renderer, camera, light, orbit, keyboard, newStructure, voxelColors, materialsIndex = 0, gui; // Initial variables
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // Init a basic renderer
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
-camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(150, 225, 300);
-orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
+const orbitCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+orbitCamera.position.set(150, 225, 300);
+orbit = new OrbitControls(orbitCamera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
+camera = orbitCamera;
 keyboard = new KeyboardState();
 voxelColors = [
   'greenyellow',
   'darkgoldenrod',
   'darkgreen'
 ]
+
+/* CONSTRUCAO CAMERA PRIMEIRA PESSOA  <<<< EM CONSTRUÇÃO !!!! >>>>>
+ TODO: 
+    1. adicionar delta (clock) para o cálculo da movimentação ser mais legível
+    2. alterar a informação de controle ao mudar de camera
+    3. retirar os eventos de click do mouse
+*/
+
+const FPCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+FPCamera.position.set(0, 50, 0);
+const FPControls = new PointerLockControls(FPCamera, renderer.domElement);
+const FPMovement = {
+    moveForward: function () {
+        FPControls.moveForward(SPEED)
+    },
+    moveBackward: function () {
+        FPControls.moveForward(-SPEED)
+    },
+    moveRight: function () {
+        FPControls.moveRight(SPEED)
+    },
+    moveLeft: function () {
+        FPControls.moveRight(-SPEED)
+    },
+    moveUp: function () {
+        FPControls.getObject().position.y += SPEED * FLY_FACTOR;
+    },
+    moveDown: function () {
+        FPControls.getObject().position.y -= SPEED * FLY_FACTOR;
+    }
+}
+
+scene.add(FPControls.getObject());
+
+/* FIM CONSTRUCAO CAMERA PRIMEIRA PESSOA */
 
 // let { materials, lineMaterials } = initMaterials(voxelColors);
 
@@ -122,6 +161,21 @@ function isFocusedOnInput() {
     return document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA';
 }
 
+function perspectiveChange(){ //AQUI DEVERÁ SER REALIZADA A LÓGICA DE TROCA DE GUI
+    if (camera === orbitCamera){
+        // document.getElementById('InfoxBox').innerHTML = "";
+        // initControlInformation(controlInfo);
+        camera = FPCamera;
+        FPControls.lock();
+    } else {
+        // document.getElementById('InfoxBox').remove();
+        // let orbitInfo = new InfoBox();
+        // initOrbitInformation(orbitInfo);
+        camera = orbitCamera;
+        FPControls.unlock();
+    }
+}
+
 function keyboardUpdate() {
 
     keyboard.update();
@@ -131,32 +185,51 @@ function keyboardUpdate() {
         return; 
     }
 
-    if (keyboard.pressed("Q")) {map.factor++; map.clear(); map.create();};
-    if (keyboard.pressed("E")) {map.factor--; map.clear(); map.create();};
-    if (keyboard.pressed("up")) {map.divisor1 += 0.01; map.clear(); map.create();};
-    if (keyboard.pressed("down")) {map.divisor1 -= 0.01; map.clear(); map.create();};
-    if (keyboard.pressed("right")) {map.divisor2 += 0.01; map.clear(); map.create();};
-    if (keyboard.pressed("left")) {map.divisor2 -= 0.01; map.clear(); map.create();};
-    if (keyboard.pressed("S") ) {map.zoff++; map.clear(); map.create();};
-    if (keyboard.pressed("W") ) {map.zoff--; map.clear(); map.create();};
-    if (keyboard.pressed("D") ) {map.xoff++; map.clear(); map.create();};
-    if (keyboard.pressed("A") ) {map.xoff--; map.clear(); map.create();};
+    if (!FPControls.isLocked) {
+        if (keyboard.pressed("Q")) {map.factor++; map.clear(); map.create();};
+        if (keyboard.pressed("E")) {map.factor--; map.clear(); map.create();};
+        if (keyboard.pressed("up")) {map.divisor1 += 0.01; map.clear(); map.create();};
+        if (keyboard.pressed("down")) {map.divisor1 -= 0.01; map.clear(); map.create();};
+        if (keyboard.pressed("right")) {map.divisor2 += 0.01; map.clear(); map.create();};
+        if (keyboard.pressed("left")) {map.divisor2 -= 0.01; map.clear(); map.create();};
+        if (keyboard.pressed("S") ) {map.zoff++; map.clear(); map.create();};
+        if (keyboard.pressed("W") ) {map.zoff--; map.clear(); map.create();};
+        if (keyboard.pressed("D") ) {map.xoff++; map.clear(); map.create();};
+        if (keyboard.pressed("A") ) {map.xoff--; map.clear(); map.create();};
+    } else { 
+        if (keyboard.pressed("W")) {FPMovement.moveForward()};
+        if (keyboard.pressed("A")) {FPMovement.moveLeft(-SPEED)};
+        if (keyboard.pressed("S")) {FPMovement.moveBackward(-SPEED)};
+        if (keyboard.pressed("D")) {FPMovement.moveRight(SPEED)};
+        if (keyboard.pressed("Q")) {FPMovement.moveUp(SPEED)};
+        if (keyboard.pressed("E")) {FPMovement.moveDown(-SPEED)};
+    }
 
-
+    if (keyboard.down("C")) {perspectiveChange()};
 }
 
-function showInformation() {
-    var controls = new InfoBox();
-    controls.add("Fator:     Q | E");
-    controls.add("X Offset:  A | D");
-    controls.add("Z Offset:  W | S");
-    controls.add("Divisor 1: ↑ | ↓");
-    controls.add("Divisor 2: ← | →");
-    controls.show();
+function initOrbitInformation() {
+    let orbitInfo = new InfoBox();
+    orbitInfo.add("Fator:     Q | E");
+    orbitInfo.add("X Offset:  A | D");
+    orbitInfo.add("Z Offset:  W | S");
+    orbitInfo.add("Divisor 1: ↑ | ↓");
+    orbitInfo.add("Divisor 2: ← | →");
+    orbitInfo.add("Câmera:        C")
+    orbitInfo.show();
     let infobox = document.getElementById('InfoxBox')
     infobox.style.fontFamily = 'Courier New, monospace';
     infobox.style.whiteSpace = 'pre';
 }
+
+// function initControlInformation(controlInfo) {
+//     controlInfo.add("Movimentação:       WASD");
+//     controlInfo.add("Voar/pousar:          QE");
+//     controlInfo.add("Mudar de perspectiva:  C");
+//     let infobox = document.getElementById('InfoxBox')
+//     infobox.style.fontFamily = 'Courier New, monospace';
+//     infobox.style.whiteSpace = 'pre';
+// }
 
 function render() {
     requestAnimationFrame(render);
@@ -165,7 +238,6 @@ function render() {
     renderer.render(scene, camera) // Render scene
 }
 
-
 buildInterface();
-showInformation();
+initOrbitInformation();
 render();
